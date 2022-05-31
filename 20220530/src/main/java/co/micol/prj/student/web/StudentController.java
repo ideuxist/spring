41c8a8e48e6d711b9dc2e471040e88c138d7bcfb
ideuxist <ideuxist@gmail.com>
2022-05-31 11:04:40 +0900

    111

--- 20220530/src/main/java/co/micol/prj/student/web/StudentController.java
+++ 20220530/src/main/java/co/micol/prj/student/web/StudentController.java
@@ -1,5 +1,8 @@
 package co.micol.prj.student.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -48,5 +51,33 @@ public class StudentController {
 			data = "Y"; //사용할 수 있는 아이디
 		}
 		return data;
+	}
+	@RequestMapping("/studentLoginForm.do")
+	public String studentLoginForm() {
+		return "student/studentLoginForm";
+	}
+	
+	@PostMapping("/studentLogin.do")
+	public String studentLogin(StudentVO vo,Model model,HttpSession session) {
+//		System.out.println(request.getParameter("id"));
+//		System.out.println(request.getParameter("name"));
+//		System.out.println("=======================");
+		
+		vo=studentDao.studentSelect(vo);
+		if(vo !=null) {
+			session.setAttribute("id", vo.getId());
+			session.setAttribute("name", vo.getName());
+			model.addAttribute("message","님 환영합니다");
+		}else {
+			model.addAttribute("message","아이디 또는 패스워드가 틀립니다");
+		}
+		
+		return "student/studentLogin";
+	}
+	@RequestMapping("/studentLogout.do")
+	public String studentLogout(HttpSession session,Model model) {
+		session.invalidate();
+		model.addAttribute("message","정상적으로 로그아웃 되었습니다");
+		return "student/studentLogout";
 	}
 }
